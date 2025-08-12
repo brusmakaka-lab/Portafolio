003ee641c097827549979f363232fdd908d3eeef
brusmakaka-lab <brusmakaka@gmail.com>
2025-08-12 10:15:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Ejemplo simple para enviar formulario
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Mensaje enviado. ¡Gracias por contactarme!");
+});
